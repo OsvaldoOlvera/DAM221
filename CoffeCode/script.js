fe0bc0daf0a17eb6772c56bd7328776aedb62ac1
OsvaldoOlvera <125052448@upq.edu.mx>
2026-09-18 23:16:29 -0600

    Parte individual de la práctica Coffee Code finalizado.
    Se finalizo el código que se estaba realizando en clase, se cambio la modalidad ya que lo tenia para mostrar en navegador y lo cambie a terminal y se elimino la parte del index.html y se quedo solo con el script.js.

--- CoffeCode/script.js
+++ CoffeCode/script.js
@@ -1,21 +1,56 @@
+const readline = require("readline");
+
+const entrada = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 let listaPedidos = [];
 
 let total = 0;
 
-function adicionarPedido() {
-    const pedido = prompt("Cual es tu pedido?");
-    const precio = prompt("Cual es el precio del pedido?");
-    listaPedidos.push(pedido);
-    total = total + precio;
+function agregarPedido(){
+    entrada.question("Cual es tu pedido? ", (pedido) => {
+        entrada.question("Cual es el precio del pedido? ", (precio) => {
+            precio =Number(precio);
+            listaPedidos.push(pedido);
+            total = total + precio;
+            console.log("Pedido agregado: " + pedido);
+            mostrarPedidos();
+            menu();
 
-    const lista =document.getElementById("listaPedidos");
-    lista.innerHTML = "";
+        });
 
-    for (let i = 0; i < listaPedidos.length; i++) {
-        const elemento = document.createElement("li");
-        elemento.textContent = listaPedidos[i];
-        lista.appendChild(elemento);
-    }
-
-    document.getElementById("total").textContent = total;
+    });
 }
+
+function mostrarPedidos() {
+    console.log("Lista de pedidos: ");
+    for (let i = 0; i < listaPedidos.length; i++) {
+        console.log((i + 1) + ". " + listaPedidos[i]);
+    }
+    console.log("Total: $" + total);
+}
+
+function menu() {
+    console.log("1. Agregar pedido");
+    console.log("2. Mostrar pedidos");
+    console.log("3. Salir");
+
+    entrada.question("Seleccione una opción: ", function(opcion) {
+        if (opcion === "1") {
+            agregarPedido();
+        } else if (opcion === "2") {
+            mostrarPedidos();
+            menu();
+        } else if (opcion === "3") {
+            console.log("Saliendo...");
+            entrada.close();
+        } else {
+            console.log("Opción no válida");
+            menu();
+        }
+    });
+}
+
+menu   ();
